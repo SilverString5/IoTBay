7172e6f4c0f201d3fbe31532af7098efbf15dd8c
Lorin-Chanel <Lorin.C.Raba@student.uts.edu.au>
2024-04-13 07:08:54 +1000

    Added Customer Class and modified Staff and User to represent supertype and subtype relationship as shown in the ERD

---
+++ ISDAssignment1/src/java/User/Customer.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package User;
+import java.io.Serializable;
+
+/**
+ *
+ * @author lorinchanel
+ */
+
+//Made into a child class in an attempt to represent subtype in ERD
+public class Customer extends User implements Serializable {
+    
+    private int customerID;
+    
+    public Customer(int customerID){
+        this.customerID = customerID;
+    }
+
+    public int getCustomerID() {
+        return customerID;
+    }
+
+    public void setCustomerID(int customerID) {
+        this.customerID = customerID;
+    }
+    
+}
--- ISDAssignment1/src/java/User/Staff.java
+++ ISDAssignment1/src/java/User/Staff.java
@@ -9,23 +9,17 @@ import java.io.Serializable;
  *
  * @author lorinchanel
  */
-public class Staff implements Serializable {
+
+//Made into a child class in an attempt to represent subtype in ERD
+public class Staff extends User implements Serializable {
 
     private int staffID;
-    private String email;
-    private String name;
-    private String phone;
-    private String password;   
-    private String address;
+    private String position;
     
     
-    public Staff(int staffID, String email, String name, String phone, String password, String address){
+    public Staff(int staffID, String position){
         this.staffID = staffID;
-        this.email = email;
-        this.name = name;
-        this.phone = phone;
-        this.password = password;
-        this.address = address;
+        this.position = position;
     }
     
     public int getStaffID() {
@@ -36,44 +30,13 @@ public class Staff implements Serializable {
         this.staffID = staffID;
     }
 
-    public String getEmail() {
-        return email;
+    public String getPosition() {
+        return position;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
+    public void setPosition(String position) {
+        this.position = position;
     }
 
-    public String getName() {
-        return name;
-    }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-    
 }

78368976a959afec2b0e6820125a6a8298713cd0
Lorin-Chanel <Lorin.C.Raba@student.uts.edu.au>
2024-04-13 22:11:21 +1000

    Edited attributes within UserAccessLogs java class

--- ISDAssignment1/src/java/uts/isd/model/UserAccessLogs.java
+++ ISDAssignment1/src/java/uts/isd/model/UserAccessLogs.java
@@ -13,17 +13,20 @@ import java.util.*;
 public class UserAccessLogs implements Serializable {
     
     private int accessLogId;
+    private int userID;
     private String browserType;
     private String eventType;
-    private Date timeStamp;
-    private int sessionNum;
+    private Date sessionCreationTime;
+    private Date sessionEndTime;
     
-    public UserAccessLogs(int accessLogId, String browserType, String eventType, Date timeStamp, int sessionNum){
+    public UserAccessLogs(int accessLogId, int userID, String browserType, String eventType, Date sessionCreationTime, Date sessionEndTime){
         this.accessLogId = accessLogId;
+        this.userID = userID;
         this.browserType = browserType;
         this.eventType = eventType;
-        this.timeStamp = timeStamp;
-        this.sessionNum = sessionNum;
+        this.sessionCreationTime = sessionCreationTime;
+        this.sessionEndTime = sessionEndTime;
+
     }
     
     public int getAccessLogId() {
@@ -32,6 +35,14 @@ public class UserAccessLogs implements Serializable {
 
     public void setAccessLogId(int AccessLogId) {
         this.accessLogId = AccessLogId;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
     }
     
     public String getBrowserType() {
@@ -50,20 +61,21 @@ public class UserAccessLogs implements Serializable {
         this.eventType = eventType;
     }
 
-    public Date getTimeStamp() {
-        return timeStamp;
+    public Date getSessionCreationTime() {
+        return sessionCreationTime;
     }
 
-    public void setTimeStamp(Date timeStamp) {
-        this.timeStamp = timeStamp;
+    public void setSessionCreationTime(Date sessionCreationTime) {
+        this.sessionCreationTime = sessionCreationTime;
+    }
+    
+    public Date getSessionEndTime() {
+        return sessionEndTime;
     }
 
-    public int getSessionNum() {
-        return sessionNum;
+    public void setSessionEndTime(Date sessionEndTime) {
+        this.sessionEndTime = sessionEndTime;
     }
 
-    public void setSessionNum(int sessionNum) {
-        this.sessionNum = sessionNum;
-    }
     
 }

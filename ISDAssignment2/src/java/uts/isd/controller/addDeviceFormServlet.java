c0e2eedda509758d1288d4a4176185f45e6c0f85
Jianliang <jianlan4200@gmail.com>
2024-05-06 12:02:12 +1000

    Upload newest working version
    Device Collection Management Done

---
+++ ISDAssignment2/src/java/uts/isd/controller/addDeviceFormServlet.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uts.isd.controller;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import uts.isd.model.Product;
+import uts.isd.model.dao.*;
+import java.util.ArrayList;
+import javax.servlet.RequestDispatcher;
+/**
+ *
+ * @author jijianlan
+ */
+public class addDeviceFormServlet extends HttpServlet{
+    
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("productForm.jsp").forward(request, response);
+    }
+    
+}

b23932652104168b0049bb6322c7fda2d27f8775
Pyae-7 <148660724+Pyae-7@users.noreply.github.com>
2024-05-09 14:53:42 +1000

    Update RemoveCartItemServlet

--- ISDAssignment1/src/java/uts/isd/controller/RemoveCartItemServlet.java
+++ ISDAssignment1/src/java/uts/isd/controller/RemoveCartItemServlet.java
@@ -7,4 +7,31 @@ package uts.isd.controller;
 
 import java.io.IOException;
 import java.util.HashMap;
-import javax.servlet.Ser
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+import uts.isd.model.*;
+import uts.isd.model.dao.*;
+import uts.isd.controller.*;
+
+/**
+ *
+ * @author pyaephyozaw
+ */
+public class RemoveCartItemServlet extends HttpServlet {
+        @Override
+        protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+            HttpSession session = request.getSession();
+            HashMap<Integer, Integer> shoppingCart = (HashMap<Integer, Integer>)session.getAttribute("shoppingCart");
+            String function = request.getParameter("function");
+            if(function.equals("RemoveAnItem")){
+                int productID = Integer.parseInt(request.getParameter("productID"));
+                if(shoppingCart.containsKey(productID)){
+                    shoppingCart.remove(productID);
+                }
+            }else if(function.equals("ClearCart")){
+                shoppingCart.clear();
+            }
+            
+            request.getRequestDispatcher("DisplayCartServlet").forward(request, response);
+        }
+}
